7d46de83d0012f20984cc767b774531128b954b4
Rhondene <rhondenewint93@gmail.com>
2017-02-11 15:00:11 -0500

    Update Naive Exact Matching.py

--- Naive Exact Matching.py
+++ Naive Exact Matching.py
@@ -20,22 +20,24 @@ def naiveMatch(t,s):                    ## s is the reference genome and t is pa
 #### as well as reverse complement of t in reference genome s. Reports
 ####  occurences where reverse complement of t = t, whihc is treated as one occurrences.
 
-def NaiveMatchRC(tc, t, s):
-    occur_t = []
-    occur_tc = []
-    for i in range(len(s)-len(t)+1):        #loops over number of alignments
-        match_t = True
-        match_tc = True
-        for j in range(len(t)):
-            if s[i+j] != t[j]:
-                match_t = False
-                break
-        for k in range(len(tc)):
-            if s[i+k] != tc[k]:
-                match_tc = False
-                break
-        if match_t:
-            occur_t.append(i)
-        if match_tc == True and tc != t:         #does not report occurence if t and its reverse complement are the same
-            occur_tc.append(i)
-    return occur_t, occur_tc
+
+def NaiveMatchRC(t,tc,s):
+	occur_t = []
+	occur_tc = []
+	for i in range(len(s)-len(t)+1):
+		match_t = True
+		for j in range(len(t)):
+			if s[i+j] != t[j]:
+				match_t = False
+				break
+		if match_t:
+			occur_t.append(i)
+	for a in range(len(s)-len(tc)+1):
+		match_tc = True
+		for k in range(len(tc)):
+			if s[a+k] != tc[k]:
+				match = False
+				break
+		if match_tc == True and tc != t:    ##will not double count if t is same as its reveerse complement
+			occur_tc.append(a)
+	return occur_t, occur_tc
