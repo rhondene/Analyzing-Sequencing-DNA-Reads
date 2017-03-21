aa88a997715e38e8f439d6c218eca27e55dcc2e7
Rhondene_fminaunt <rhondenewint93@gmail.com>
2017-03-21 09:23:10 -0500

    Update Boyer-Moore - Exact and Approximate.py
    I added extra comments to see how well I gasp the intuition behind the algorithm. RWint

--- Boyer-Moore - Exact and Approximate.py
+++ Boyer-Moore - Exact and Approximate.py
@@ -30,34 +30,38 @@ def boyer_moore(t, t_bm, s):  ##t is pattern and s is reference genome
     return occur, num
     
     
- ###Approximate Matching using modified Boyer-Moore
+ ###Approximate Matching By applying K+1 PigeonHole Principle to  Boyer-Moore
+             #1.divide pattern t into mm+1 segments (mm =  k edits = mismatches allowed)
+               #2. create an outer loop to apply approximate matching to an ith segment
+                #3.mInside the outer for: apply preprocesing and approx matching to  that ith segment
+                #4. Record and Store indices of matches
 
-def approx_match(t, s, n ):##n is number of mismtaches
+def approx_match(t, s, mm ):    ##mm is number of mismtaches
     
-    segment_length = int(round(len(t) / (n+1)))   ##computes length of each n+1 segment
-    all_matches = set()            ##does not allow duplicates
-    for i in range(n+1):      ## the outerloop handles matches in each ith segment
+    segment_length = int(round(len(t) / (mm+1)))   ##computes length of each mm+1 segment
+    all_matches = set()                             ##using set() to avoid  duplicates
+    for i in range(mm+1):                            ## the outerloop applies boyer-moore to  each i-th segment
         start = i*segment_length
-        end = min((i+1)*segment_length, len(t))  ##so that segment does not run past len of t
-        t_bm = BoyerMoore(t[start:end], alphabet = 'AGCT')  ##does preprocessing tables for gs rule and bc rule
-        matches = boyer_moore(t[start:end], t_bm, s)   ##returns where segment of t matches refeence text s
+        end = min((i+1)*segment_length, len(t))                 ##so that segment does not run past len of t
+        t_bm = BoyerMoore(t[start:end], alphabet = 'AGCT')      ##does preprocessing tables for gs rule and bc rule
+        matches = boyer_moore(t[start:end], t_bm, s)            ##where segment of t matches refeence text s is an index hit, 
         
-        for m in matches:
-            if m < start or m-start+len(t) > len(s):
-                continue
+        for m in matches:                                          #perform verification at each index hit m, where segment of t matched s
+            if m < start or m-start+len(t) > len(s):                #ensure matches are not out of scope of length? like -1 or len s+1
+                continue                                            ##could use assert keyword instead of if....continue (me a learn!!)
                 
             mismatches = 0
-            for j in range(0,start):     ##for the jth alignment
+            for j in range(0,start):                ##at the m-start--th  alignement, compare the jth character
                 if t[j] != s[m-start+j]:
                     mismatches += 1
-                    if mismatches > n:
+                    if mismatches > mm:          #once mismatches surpass mm, no need to continue
                         break
             for j in range(end,len(t)):   
                 if t[j] != s[m-start+j]:
                     mismatches += 1
-                    if mismatches > n:
+                    if mismatches > mm:
                         break
-            if mismatches <= n:
-                all_matches.add(m - start)
+            if mismatches <= mm:
+                all_matches.add(m - start)         ##add that alignment =  offset of match in s
     return list(all_matches)  ##turns the set all_match into an indexed list
     
